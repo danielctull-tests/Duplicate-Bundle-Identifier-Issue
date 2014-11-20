cb9681a87ca4db3c2cc25455099f37af4e611d44
Daniel Tull <dt@danieltull.co.uk>
2014-11-20 17:31:57 +0000

    Initial commit

---
+++ Amazing Framework/Amazing.h
@@ -0,0 +1,14 @@
+//
+//  Amazing Framework.h
+//  Amazing Framework
+//
+//  Created by Daniel Tull on 20.11.2014.
+//  Copyright (c) 2014 Daniel Tull. All rights reserved.
+//
+
+@import Foundation;
+
+//! Project version number for Amazing Framework.
+FOUNDATION_EXPORT double AmazingVersionNumber;
+FOUNDATION_EXPORT const unsigned char AmazingVersionString[];
+
---
+++ Amazing/AppDelegate.m
@@ -0,0 +1,12 @@
+//
+//  AppDelegate.m
+//  Amazing
+//
+//  Created by Daniel Tull on 20.11.2014.
+//  Copyright (c) 2014 Daniel Tull. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+@end
